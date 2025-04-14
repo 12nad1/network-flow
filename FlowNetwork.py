a1be44f3dbb70678933633f0fd9f47c09175cd47
Maxewell Kaye <32784707+12nad1@users.noreply.github.com>
2025-04-14 11:48:22 -0400

    made mor modular, added function skeletons

--- FlowNetwork.py
+++ FlowNetwork.py
@@ -1,5 +1,6 @@
 import xarray as xr
 import networkx as nx
+import numpy as np
 
 '''
 netcdf_path is a dataset with lat lon variables (no time)
@@ -8,7 +9,7 @@ inflow_var is the data_var that stores in-flow data
 '''
 class FlowNetwork:
 	def __init__(self, netcdf_path, outflow_var, inflow_var, thresh=0, time=None):
-		self.edge_distribution = nx.DiGraph()
+		self.ensemble = nx.DiGraph()
 		self.graph = nx.DiGraph()
 		self.ds = xr.load_dataset(netcdf_path)
 		if time:
@@ -18,13 +19,42 @@ class FlowNetwork:
 		self.thresh = thresh
 		self.distances = xr.load_dataset('distances.nc')
 
-	def generate_weighted_edges(self, model):
+	'''
+	Main driver for the network generation. First generates the ensemble probability distribution and then samples the graph
+	'''
+	def run_model(self, ensemble_model = 'gravity', graph_model = 'config'):
+		self.generate_ensemble(ensemble_model)
+		self.sample_ensemble(graph_model)
+
+	'''
+	Generates the ensemble
+	'''
+	def generate_ensemble(self, model):
 		if model == 'gravity':
 			self.gravity_model(self)
-			
+
+
+	'''
+	Samples the edges of the graph based on the existing ensebmle
+	'''
+	def sample_ensemble(self, model):
+		if self.
+		if model == 'config':
+			self.config(self)
+
 	# returns the distance between the pair of coordinates
 	def dist(self, coord1, coord2):
-		return self.distances.sel(coord1['lat'], coord2['lat'], coord1['lon'], coord2['lon'])
+		# Convert degrees to radians
+		lat1, lon1 = np.radians(coord1['lat']), np.radians(coord1['lon'])
+		lat2, lon2 = np.radians(coord2['lat']), np.radians(coord2['lon'])
+		
+		angle_delta = np.arccos(
+			np.sin(lat1) * np.sin(lat2) +
+			np.cos(lat1) * np.cos(lat2) * np.cos(lon2 - lon1)
+		)
+
+		return angle_delta * 6371
+
 
 	# Generates edges between pairs of grid cells in self.ds and stores them in self.graph, overwriting any previously generated edges
 	# Edge probability is proprtional to m1 * m2 / r
